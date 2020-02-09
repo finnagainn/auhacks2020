4261691ec80505cb8959bd3d39dd6f95fca19256
ChrisAcosta15 <44481494+ChrisAcosta15@users.noreply.github.com>
2020-02-08 19:16:17 -0600

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,41 @@
+public class Company {
+	private String name;
+	private String website;
+	private String description;
+	private String job;
+	private String jobReq;
+
+	public Company(String nameIn, String websiteIn, String descriptionIn, String jobIn, String jobReqIn) {
+
+	}
+	public void setName(String nameIn) {
+		name = nameIn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setWebsite(String websiteIn) {
+		website = websiteIn;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setDescription(String descriptionIn) {
+		description = descriptionIn;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setJob(String jobIn) {
+		job = jobIn;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJobReq(String jobReqIn) {
+		jobReq = jobReqIn;
+	}
+	public String getJobReq() {
+		return jobReq;
+	}
+}
